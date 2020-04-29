f5d6250bfd53d70b6b3ddb31806ca09c5bb2447b
COMHERE <63200008+COMHERE@users.noreply.github.com>
2020-04-29 20:35:49 +0700

    Update data_ikpm.js

--- data_ikpm.js
+++ data_ikpm.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
 
             var table = $('#data_ikpm_kediri').DataTable({
                 "scrollX": true,
-                  "scrollY":        "200px",
-                 "scrollCollapse": true,
+                "scrollY": "200px",
+                "scrollCollapse": true,
                 "responsive": true,
                 "searchable": false,
                 "orderable": false,
