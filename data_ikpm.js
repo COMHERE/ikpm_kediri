d119c0a3ea11b1dda020a5484b87805876faec48
COMHERE <63200008+COMHERE@users.noreply.github.com>
2020-04-29 20:06:51 +0700

    Update data_ikpm.js

--- data_ikpm.js
+++ data_ikpm.js
@@ -1,82 +1,62 @@
-function format($t) {
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
-        '<tr>' +
-        '<td>Jenis Kelamin:</td>' +
-        '<td>' + $t.gsx$jeniskelamin.$t + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>Alamat:</td>' +
-        '<td>' + $t.gsx$alamatrumah.$t + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>Pekerjaan:</td>' +
-        '<td>' + $t.gsx$profesikerja.$t + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>Bisnis:</td>' +
-        '<td>' + $t.gsx$jenisusahabisnis.$t + '</td>' +
-        '</tr>' +
-        '<td>Karya/Prestasi:</td>' +
-        '<td>' + $t.gsx$karyaprestasilain.$t + '</td>' +
-        '</tr>' +
-        '</table>';
-}
+$(document).ready(function() {
+            if ($(window).width() <= 1024) {
+                $("#data_ikpm_kediri").attr('class', '');
+            } else {
+                $("#data_ikpm_kediri").attr('class', 'nowrap');
+            }
 
-$(document).ready(function () {
-    var table = $('#data_ikpm_kediri').DataTable({
-        "responsive": true,
-        "searchable": false,
-        "orderable": false,
-        "target": 0,
-        "bServerSide": false,
-        "bProcessing": true,
-        "sAjaxDataProp": "feed.entry",
-        "sAjaxSource": "https://spreadsheets.google.com/feeds/list/19O0NeamiZGJ8tpqSRw37hVY7yOh9TVEg4TSVj4WtdMs/1/public/values?alt=json",
-        "aoColumns": [{
-            "mDataProp": null
-        },
-        {
-            "mDataProp": "gsx$namalengkapanggota.$t"
-        },
-        {
-            "mDataProp": "gsx$tahunangkatanalumnigontor.$t"
-        },
-        {
-            "mDataProp": "gsx$nostambuk.$t"
-        },
-        {
-            "className": 'details-control',
-            "orderable": false,
-            "data": null,
-            "defaultContent": ''
-        }
-        ],
-        "order": [
-            [1, 'asc']
-        ]
-    });
+            var table = $('#data_ikpm_kediri').DataTable({
+                "scrollX": true,
+                "responsive": true,
+                "searchable": false,
+                "orderable": false,
+                "bServerSide": false,
+                "bProcessing": true,
+                "sAjaxDataProp": "feed.entry",
+                "sAjaxSource": "https://spreadsheets.google.com/feeds/list/19O0NeamiZGJ8tpqSRw37hVY7yOh9TVEg4TSVj4WtdMs/1/public/values?alt=json",
+                "aoColumns": [{
+                        "mDataProp": null
+                    },
+                    {
+                        "mDataProp": "gsx$namalengkapanggota.$t"
+                    },
+                    {
+                        "mDataProp": "gsx$tahunangkatanalumnigontor.$t"
+                    },
+                    {
+                        "mDataProp": "gsx$nostambuk.$t"
+                    },
+                    {
+                        "mDataProp": "gsx$jeniskelamin.$t"
+                    },
+                    {
+                        "mDataProp": "gsx$profesikerja.$t"
+                    },
+                    {
+                        "mDataProp": "gsx$jenisusahabisnis.$t"
+                    },
+                    {
+                        "mDataProp": "gsx$karyaprestasilain.$t"
+                    },
+                    {
+                        "mDataProp": "gsx$alamatrumah.$t"
+                    },
 
-    table.on('order.dt search.dt', function () {
-        table.column(0, {
-            search: 'applied',
-            order: 'applied'
-        }).nodes().each(function (cell, i) {
-            cell.innerHTML = i + 1;
+                ],
+                order: [0, 'asc'],
+
+                fixedHeader: {
+                    headerOffset: 50
+                }
+            });
+
+            table.on('order.dt search.dt', function() {
+                table.column(0, {
+                    search: 'applied',
+                    order: 'applied'
+                }).nodes().each(function(cell, i) {
+                    cell.innerHTML = i + 1;
+                });
+            }).draw();
+
         });
-    }).draw();
-
-    $('#data_ikpm_kediri tbody').on('click', 'td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = table.row(tr);
-
-        if (row.child.isShown()) {
-
-            row.child.hide();
-            tr.removeClass('shown');
-        } else {
-
-            row.child(format(row.data())).show();
-            tr.addClass('shown');
-        }
-    });
-});
